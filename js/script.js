cfc7fb5ee79edb0e086062b460ac510a6eaa6f59
Razvan <razvanrus289@yahoo.com>
2019-09-27 10:31:56 -0500

    hid the pictures on mobile and table views

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-function init() {
+window.addEventListener('load', function init()) {
 
-  var button = document.getElementByTagName("submit");
+  var button = document.getElementById("submit");
 
-  var fname =  document.getElementByTagName("first_name_field");
+  var fname =  document.getElementById("first_name_field");
 
-  var lname = document.getElementByTagName("last_name_field");
+  var lname = document.getElementById("last_name_field");
 
   button.addEventListener("click", function() {
     alert (fname.value + lname.value + "Thank you for your intrest in adopting a pet! We will reach out to you as soon as one of our representatives reviews your application!");
-  })
+  }
 }
-window.addEventListener('load', init);
