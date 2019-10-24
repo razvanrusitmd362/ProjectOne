d78a962245452be4eca1f04c96d6b0a6391ea91b
Razvan <razvanrus289@yahoo.com>
2019-10-24 16:48:23 -0500

    rewrote js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
+function init() {
 
-window.addEventListener('load', function init()) {
+var button = document.getElementById("input"); /* what I click */
 
-  var button = document.getElementById("submit");
+var fname = document.getElementById("first_name_field"); /* where I type*/
 
-  var fname =  document.getElementById("first_name_field");
+var lname = document.getElementById("last_name_field"); /* where I type*/
 
-  var lname = document.getElementById("last_name_field");
 
-  button.onclick("click", function() {
-    alert (fname.value + lname.value + "Thank you for your intrest in adopting a pet! We will reach out to you as soon as one of our representatives reviews your application!");
-  }
+button.addEventListener("click", function() {
+  alert (fname.value + lname.value);
+});
 }
-
-
-/*
-document.getElementClassName("submit").addEventListener("click", myFunction);
-
-function myFunction() {
-  document.getElementByClassBane("submit").innerHTML = "YOU CLICKED ME!";
-}
-*/
+window.addEventListener('load', init);
