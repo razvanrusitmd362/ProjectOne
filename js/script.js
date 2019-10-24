0a3ecb5b56cc53c8c79446dac83a2ec2398aa060
Razvan <razvanrus289@yahoo.com>
2019-10-24 17:05:24 -0500

    got js to load

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ var fname = document.getElementById("first_name_field"); /* where I type*/
 
 var lname = document.getElementById("last_name_field"); /* where I type*/
 
+var email = document.getElementById("email_field"); /* where I type*/
 
 button.addEventListener("click", function() {
-  alert (fname.value + lname.value);
+  alert ("Thank you! You will be contacted with more information about your inquiry");
 });
 }
 window.addEventListener('load', init);
